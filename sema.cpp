ef945448c338435f882a29722600dbaf32bdcabf
Roland Leißa <leissa@cs.uni-saarland.de>
2013-03-21 17:13:00 +0100

    added trackers in order to track definitions after Def::replace

--- sema.cpp
+++ sema.cpp
@@ -195,7 +195,7 @@ const Type* Id::vcheck(Sema& sema) const {
     if (const Decl* decl = sema.lookup(symbol())) {
         decl_ = decl;
 
-#if 0
+#if 1
         if (const VarDecl* vardecl = decl->isa<VarDecl>()) {
             if (!vardecl->type()->isa<Pi>() && !vardecl->type()->is_generic())
                 vardecl->is_address_taken_ = true;
