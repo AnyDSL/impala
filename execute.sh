4c4ad27c76e4ba1692b73bbadea7c94f5e1a580f
Roland Leißa <leissa@cs.uni-saarland.de>
2013-08-21 15:50:49 +0200

    fixed execute.sh script

--- execute.sh
+++ execute.sh
@@ -2,7 +2,7 @@
 # parameter 1: Impala program with a 'foo' function that returns an int
 rm -f a.out
 # generate LLVM code
-anydsl2 -i $1 --emit-llvm -O >& out.ll
+./impala -i $1 --emit-llvm -O >& out.ll
 # generate assembler
 llc out.ll
 # link with a C main function
