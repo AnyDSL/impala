AnyDSL/impala

commit da2baf2f9d8f3850e66fa29f6b35563975cc28f1

fixed todo

type.cpp
@@ -150,8 +150,9 @@ void TypeTable::insert_new(const Type* type) {
 
     type->set_representative(type);
 
-    // TODO is this a correct instanceof test? -- maybe use a virtual method instead?
-    if (!type->isa<TypeVar>()) {
+    if (type->kind() == Type_var) {
+        // TODO is this a correct instanceof test?
+        assert(!type->isa<TypeVar>());
         auto p = types_.insert(type);
         assert(p.second && "hash/equal broken");
     }

commit 2bdba622aeeab0e8fed5dcffa23ac75c2ffd7067

fixed bug introduced by da2baf2f9d8f38

type.cpp
@@ -179,9 +179,9 @@ void TypeTable::insert_new(const Type* type) {
 
     type->set_representative(type);
 
-    if (type->kind() == Type_var) {
+    if (type->kind() != Type_var) {
         // TODO is this a correct instanceof test?
-        assert(type->isa<TypeVar>());
+        assert(!type->isa<TypeVar>());
         auto p = types_.insert(type);
         assert(p.second && "hash/equal broken");
     }