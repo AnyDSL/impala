98d8fbf778814e33b0608df22994c6d77fe49e9f
Roland Leißa <leissa@cs.uni-saarland.de>
2013-11-06 00:55:15 +0100

    WIP

--- main.cpp
+++ main.cpp
@@ -98,7 +98,7 @@ int main(int argc, char** argv) {
 
             init.world.breakpoint(num);
         }
-        //init.world.breakpoint(80);
+        //init.world.breakpoint(82);
 #endif
 
         anydsl2::AutoPtr<impala::Scope> prg = new impala::Scope();
