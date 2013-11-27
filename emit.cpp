a1e449fc479b263b9010e151cb874ef611c46d31
Marcel Köster <koester@cs.uni-saarland.de>
2013-11-27 15:01:47 +0100

    Hacked possible support for different accelerators.

--- emit.cpp
+++ emit.cpp
@@ -67,8 +67,8 @@ bool CodeGen::emit_prg(const Scope* prg) {
             lambda->attribute().set(Lambda::Extern);
             lambda->name = proto->symbol().str();
             // HACK: eliminate this hack
-            if (lambda->name == "all_on_gpu")
-                lambda->attribute().set(Lambda::Cuda);
+            if (lambda->name == "accelerator")
+                lambda->attribute().set(Lambda::Accelerator);
             if (lambda->name == "stencil_array")
                 lambda->attribute().set(Lambda::StencilAr);
             if (lambda->name == "array")
