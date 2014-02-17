748e3ebde31fb20172c2d01c5985ed83ec5c382d
David Poetzsch-Heffter <davidpoetzsch@web.de>
2014-02-17 21:22:28 +0100

    customized run_tests.py script

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,26 +1,27 @@
+#!/usr/bin/env python
+
 """
 This script can be used to execute the tests. 
 
-Call it with:
- -e <Path to you executable>
- -g either gruppe42 or cc-1 to execute the tests by gruppe42/cc-1 respectively. 
- 
-To add your own tests, create a tests.py in your group directory, after that, -g <your group> should work. 
-Have a look at gruppe42/tests.py to see how to write a tests.py file. 
+Command line options:
+ -e, --executable <Path to executable>
+ -d, --directory <subdirectory that contains a group of tests>  (Mandatory)
+ --disable-progressbar Disable the fancy progress bar
 """
 
 import infrastructure.tests
 import os, sys, getopt, imp
 
-def invoke(executable, group, pb):
-    tests = imp.load_source("tests", os.path.join(group, "tests.py"))
+def invoke(executable, directory, pb):
+    tests = imp.load_source("tests", os.path.join(directory, "tests.py"))
     infrastructure.tests.executeTests(tests.allTests(), executable, pb)
 
 def main():
+    executable = "../build/bin/impala"
     pb = True
     # get cmd file
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "he:g:", ["help", "executable", "group","disable-progressbar"])
+        opts, args = getopt.getopt(sys.argv[1:], "he:d:", ["help", "executable", "directory","disable-progressbar"])
     except getopt.error as msg:
         print(msg)
         sys.exit(2)
@@ -29,22 +30,17 @@ def main():
         if o in ("-h", "--help"):
             print(__doc__)
             sys.exit(0)
-        if o in ("-g", "--group"):
-            group = a
+        if o in ("-d", "--directory"):
+            directory = a
         if o in ("-e", "--executable"):
             executable = a
-        if o in ("-p", "--prepared"):
-            p = True
         if o in ("--disable-progressbar"):
             pb = False
 
-    if not 'group' in locals():
-        print("You did not specify the -g option.")
-        print(__doc__)
-    elif not 'executable' in locals():
-        print("You did not specify the -e option.")
+    if not 'directory' in locals():
+        print("You did not specify the -d option.")
         print(__doc__)
     else:
-        invoke(executable, group, pb)
+        invoke(executable, directory, pb)
 
 main()
