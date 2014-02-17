b648fdc909b5ec3aec805514ecb99d3e27d75f98
David Poetzsch-Heffter <davidpoetzsch@web.de>
2014-02-17 22:36:51 +0100

    simpler cli

--- test/run_tests.py
+++ test/run_tests.py
@@ -3,9 +3,10 @@
 """
 This script can be used to execute the tests.
 
+Usage: run_tests.py [options] <subdirectory that contains test cases>
+
 Command line options:
  -e, --executable <Path to executable>
- -d, --directory <subdirectory that contains a group of tests>  (Mandatory)
  --disable-progressbar Disable the fancy progress bar
 """
 
@@ -13,34 +14,43 @@ import infrastructure.tests
 import os, sys, getopt, imp
 
 def invoke(executable, directory, pb):
-    tests = imp.load_source("tests", os.path.join(directory, "tests.py"))
-    infrastructure.tests.executeTests(tests.allTests(), executable, pb)
+    testfile = os.path.join(directory, "tests.py")
+    
+    if os.path.exists(testfile):
+        tests = imp.load_source("tests", testfile).allTests()
+    else:
+        tests = infrastructure.tests.make_tests(directory)
+        
+    infrastructure.tests.executeTests(tests, executable, pb)
 
 def main():
     executable = "../build/bin/impala"
     pb = True
+    
+    if len(sys.argv) < 2:
+        print("You did not specify a test directory.")
+        print(__doc__)
+        sys.exit(2)
+    else:
+        directory = sys.argv[-1]
+    
     # get cmd file
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "he:d:", ["help", "executable", "directory","disable-progressbar"])
+        opts, args = getopt.getopt(sys.argv[1:-1], "he:", ["help", "executable", "disable-progressbar"])
     except getopt.error as msg:
         print(msg)
         sys.exit(2)
+    
     # handle options
     for o, a in opts:
         if o in ("-h", "--help"):
             print(__doc__)
             sys.exit(0)
-        if o in ("-d", "--directory"):
-            directory = a
         if o in ("-e", "--executable"):
             executable = a
         if o in ("--disable-progressbar"):
             pb = False
 
-    if not 'directory' in locals():
-        print("You did not specify the -d option.")
-        print(__doc__)
-    else:
-        invoke(executable, directory, pb)
+    invoke(executable, directory, pb)
 
 main()
