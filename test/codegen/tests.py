cd9c978df57345a0f6e5dd1f54f23541fe985801
David Poetzsch-Heffter <davidpoetzsch@web.de>
2014-08-19 15:58:24 +0200

    started porting codegen tests to match testing infrastructure

--- test/codegen/tests.py
+++ test/codegen/tests.py
@@ -3,13 +3,19 @@ tests.py for codegen
 """
 
 # import the test infrastructure
-from infrastructure.tests import make_tests
-import os
+from infrastructure.tests import make_invoke_tests
+
+optionals = []
 
 def allTests():
     """
     This function returns a list of tests.
     """
-    tests = make_tests("codegen", True, ["--emit-llvm"])
+    tests = make_invoke_tests("codegen")
+    
+    # mark optionals
+    for test in tests:
+        if test.getName() in optionals:
+            test.opt()
     
     return tests
