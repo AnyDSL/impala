c83c442db12ca04debccd83f3cfba25391199c7e
Roland Leißa <leissa@cs.uni-saarland.de>
2014-08-27 19:20:43 +0200

    added nullptr test

--- test/infrastructure/call_impala_main.c
+++ test/infrastructure/call_impala_main.c
@@ -3,6 +3,12 @@
 
 int main_impala();
 
+void* thorin_malloc(size_t size) { 
+    void* p;
+    posix_memalign(&p, 64, size);
+    return p;
+}
+
 void println(const char* s) {
     printf("%s\n", s);
 }
