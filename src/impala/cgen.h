d5745d607df342dba02a262ff35ae598e2da704c
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2014-11-25 12:15:11 +0100

    C interface generation integrated into impala + minor changes to opions in main.cpp

---
+++ src/impala/cgen.h
@@ -0,0 +1,36 @@
+#ifndef IMPALA_CGEN_H
+#define IMPALA_CGEN_H
+
+#include <iostream>
+
+namespace impala {
+
+struct CGenOptions {
+    CGenOptions()
+        : structs_only(false)
+        , fns_only(false)
+        , file_name("interface.h")
+        , guard("INTERFACE_H")
+    {}
+
+    bool structs_only : 1;
+    bool fns_only : 1;
+    std::string file_name;
+    std::string guard;
+};
+
+/** 
+ * @brief Generates a C interface from the contents of an Impala module.
+ *  The typechecking pass has to be run before a call to this function.
+ * 
+ * @param mod The module contents.
+ * @param opts C interface generation options.
+ * @param o The stream to use as output.
+ * @return true on success, otherwise false
+ */
+bool generate_c_interface(const ModContents* mod, const CGenOptions& opts = CGenOptions(), std::ostream& o = std::cout);
+
+}
+
+#endif
+
