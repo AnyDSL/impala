9746ee7cd2ebe52f73fc10767269dccfca55caf6
Tobias Frey <stud.uni-saarland.de>
2015-05-17 11:32:22 +0200

    - added benchmarks to the test suit

--- test/codegen/benchmarks/tests.py
+++ test/codegen/benchmarks/tests.py
@@ -9,15 +9,38 @@ optionals = [
 
 ]
 
+args = {
+    "codegen/benchmarks/aobench.impala" : [],
+    "codegen/benchmarks/fannkuch.impala" : ["10"],
+    "codegen/benchmarks/fasta.impala" : ["500000"],
+    "codegen/benchmarks/mandelbrot.impala" : ["3000"],
+    "codegen/benchmarks/meteor.impala" : ["2098"],
+    "codegen/benchmarks/nbody.impala" : ["6000000"],
+    "codegen/benchmarks/pidigits.impala" : ["10000"],
+    "codegen/benchmarks/regex.impala" : [],
+    "codegen/benchmarks/reverse.impala" : [],
+    "codegen/benchmarks/spectral.impala" : ["1800"],
+}
+
+inputs = {
+    "regex.impala" : "codegen/benchmarks/input_regex.txt",
+    "reverse.impala" : "codegen/benchmarks/input_reverse.txt",
+}
+
 def allTests():
     """
     This function returns a list of tests.
     """
-    tests = make_invoke_tests("codegen/benchmarks")
+    tests = make_invoke_tests("codegen/benchmarks", [], True, {"aobench.impala" : "ao.ppm"}, inputs)
     
     # mark optionals
     for test in tests:
         if test.getName() in optionals:
             test.opt()
+
+        if test.getName() in args:
+            test.args = args[test.getName()]
+
+
     
     return tests
