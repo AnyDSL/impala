41631c00bd3c8b4a689c54d2913f50b7820ad53c
Roland Leißa <leissa@cs.uni-saarland.de>
2015-08-06 17:36:15 +0200

    adding forward=false variants

--- src/impala/main.cpp
+++ src/impala/main.cpp
@@ -217,16 +217,16 @@ int main(int argc, char** argv) {
                 DomTree::emit_world(init.world);
                 std::cout << "Printing LoopTree: (true)" << std::endl;
                 LoopTree<true>::emit_world(init.world);
-                //std::cout << "Printing LoopTree: (false)" << std::endl;
-                //LoopTree<false>::emit_world(init.world);
+                std::cout << "Printing LoopTree: (false)" << std::endl;
+                LoopTree<false>::emit_world(init.world);
                 std::cout << "Printing CFG: (true)" << std::endl;
                 CFG<true>::emit_world(init.world);
-                //std::cout << "Printing CFG: (false)" << std::endl;
-                //CFG<false>::emit_world(init.world);
+                std::cout << "Printing CFG: (false)" << std::endl;
+                CFG<false>::emit_world(init.world);
                 std::cout << "Printing DFG: (true)" << std::endl;
                 DFGBase<true>::emit_world(init.world);
-                //std::cout << "Printing DFG: (false)" << std::endl;
-                //DFGBase<false>::emit_world(init.world);
+                std::cout << "Printing DFG: (false)" << std::endl;
+                DFGBase<false>::emit_world(init.world);
             }
         } else
             return EXIT_FAILURE;
