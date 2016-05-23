af80b97bde03c1b8d67c94da76df8882f7a50bc6
Klaas Boesche <boesche@cs.uni-saarland.de>
2016-05-23 14:54:09 +0200

    python3 compatibility for test framework

--- test/infrastructure/tests.py
+++ test/infrastructure/tests.py
@@ -4,66 +4,67 @@ Created on 8 Dec 2013
 @author: Alexander Kampmann, David Poetzsch-Heffter
 '''
 
+from __future__ import absolute_import
 import sys, os, difflib, shutil, imp, tempfile
-from timed_process import CompileProcess, RuntimeProcess
-from valgrindxml import ValgrindXML
+from .timed_process import CompileProcess, RuntimeProcess
+from .valgrindxml import ValgrindXML
 
 class Test(object):
     """Superclass for all the tests."""
     optional = False
-    
+
     def __init__(self, base, src, options, optional=False):
         self.basedir = base
         self.srcfile = src
         self.options = options
         self.optional = optional
-    
+
     def opt(self):
         self.optional = True
         return self
-        
+
     def isOptional(self):
         return self.optional
-    
+
     def getName(self):
         return os.path.join(self.basedir, self.srcfile)
-    
+
     def checkBasics(self, proc):
         cmd = os.path.basename(proc.cmd[0])
-        
+
         if proc.killed:
             print("[FAIL] " + os.path.join(self.basedir, self.srcfile))
             print("  Process '%s' timed out." % cmd)
             print
             return False
-        
+
         if proc.crash():
             print("[FAIL] " + os.path.join(self.basedir, self.srcfile))
             print("  '%s' crashed. Return code was: %d" % (cmd, proc.returncode))
             print
             return False
-        
+
         return True
-    
+
 class ValgrindTest(Test):
     VALGRIND_XML_FILE = os.path.join(tempfile.gettempdir(), "impala_valgrind.xml")
     VALGRIND_TIMEOUT_FACTOR = 5
-    
+
     def __init__(self, test):
         super(ValgrindTest, self).__init__(test.basedir, test.srcfile, test.options, test.isOptional())
-    
+
     def invoke(self, gEx):
         execCmd = ["valgrind", "--xml=yes", "--xml-file="+ValgrindTest.VALGRIND_XML_FILE]
         execCmd += [gEx] + self.options + [self.srcfile]
-        
+
         timeout = CompileProcess.timeout
         if timeout == CompileProcess.DEFAULT_TIMEOUT:
             timeout *= ValgrindTest.VALGRIND_TIMEOUT_FACTOR
-        
+
         p = CompileProcess(execCmd, self.basedir, timeout)
         p.execute()
         return self.checkBasics(p) and self.checkValgrind(p)
-    
+
     def checkValgrind(self, p):
         try:
             vgout = ValgrindXML(ValgrindTest.VALGRIND_XML_FILE)
@@ -74,7 +75,7 @@ class ValgrindTest(Test):
                 print("[FAIL] " + os.path.join(self.basedir, self.srcfile))
                 print
                 print(vgout)
-            
+
             return success
         except Exception as e:
             print("Parsing valgrind output FAILED: %s" % e)
@@ -83,14 +84,14 @@ class ValgrindTest(Test):
 def diff_output(output, expected):
     olines = output.splitlines(1)
     elines = expected.splitlines(1)
-    
+
     diff = difflib.Differ()
     fails = 0
     for cp in diff.compare(elines, olines):
         if cp.startswith('-') or cp.startswith('+'):
             print(cp.rstrip())
             fails=fails+1
-    
+
     return True if fails == 0 else False
 
 class CompilerOutputTest(Test):
@@ -100,12 +101,12 @@ class CompilerOutputTest(Test):
     srcfile = ""
     options = ""
     result = None
-    
+
     def __init__(self, positive, base, src, res, options=[]):
         super(CompilerOutputTest, self).__init__(base, src, options)
         self.positive = positive
         self.result = res
-    
+
     def invoke(self, gEx):
         execCmd = [gEx] + self.options + [self.srcfile]
         p = CompileProcess(execCmd, self.basedir)
@@ -113,17 +114,18 @@ class CompilerOutputTest(Test):
         return self.checkBasics(p) and self.checkOutput(p)
 
     def checkOutput(self, p):
+        decoded = p.output.decode()
         if p.success() != self.positive:
             print("[FAIL] "+os.path.join(self.basedir, self.srcfile))
-            print("Output: "+p.output)
+            print("Output: "+decoded)
             print
             return False
-        
+
         if self.result is None:
             return True
-    
+
         with open(os.path.join(self.basedir, self.result), 'r') as f:
-            return diff_output(p.output, f.read())
+            return diff_output(decoded, f.read())
 
 class InvokeTest(Test):
     """Superclass tests which work on a single file and compare the output."""
@@ -138,11 +140,11 @@ class InvokeTest(Test):
     args = None
 
     LIB_C = os.path.join(os.path.dirname(__file__), "lib.c")
-    
+
     def __init__(self, base, src, output_file, options=[], benchmarks=False, compare=None, input_file=None):
         super(InvokeTest, self).__init__(base, src, options+["-emit-llvm"])
         self.output_file = output_file
-        
+
         basename = os.path.splitext(self.srcfile)[0]
         self.ll_file = basename + ".ll"
         self.bc_file = basename + ".bc"
@@ -156,7 +158,7 @@ class InvokeTest(Test):
             self.tmp_files.append(self.compare_file)
 
         self.input_file = input_file
-    
+
     def compilePhases(self, gEx):
         yield [gEx] + self.options + [os.path.join(self.basedir, self.srcfile)]
         if(self.benchmarks):
@@ -166,7 +168,7 @@ class InvokeTest(Test):
             yield ["llc", "-o", self.s_file, self.bc_file]
             yield ["cc", "-o", self.exe_file, self.s_file, InvokeTest.LIB_C]
 
-    
+
     def invoke(self, gEx):
         # if any tmp file already exists do not touch it and fail
         #for tmp in self.tmp_files:
@@ -182,7 +184,7 @@ class InvokeTest(Test):
                 p.execute()
                 if not (self.checkBasics(p) and self.compilationSuccess(p)):
                     return False
-            
+
             # run executable
             if self.args is None:
                 p = RuntimeProcess([os.path.join(".", self.exe_file)], ".")
@@ -201,14 +203,14 @@ class InvokeTest(Test):
             for tmp in self.tmp_files:
                 self.cleanup(tmp)
                 pass
-    
+
     def checkOutput(self, proc):
         if not proc.success():
             print("[FAIL] "+os.path.join(self.basedir, self.srcfile))
             print("  Return code was '%d'" % proc.returncode)
             print
             return False
-        
+
         if self.output_file is not None:
             if self.compare_file is None:
                 #Image.open();
@@ -219,7 +221,7 @@ class InvokeTest(Test):
                     with open(os.path.join(self.basedir, self.output_file), 'r') as g:
                         return diff_output(f.read(), g.read())
         return True
-            
+
     def cleanup(self, file):
         if os.path.exists(file):
             os.remove(file)
@@ -227,16 +229,16 @@ class InvokeTest(Test):
     def compilationSuccess(self, p):
         if not p.success():
             print("\n[FAIL] "+os.path.join(self.basedir, self.srcfile))
-            print("Output: "+p.output)
+            print("Output: "+p.output.decode())
             return False
         return True
 
 def get_tests(directory):
     """A generator for test files based on the .impala files in directory
-    
+
     Output files are expected to have the same name but with .output extension.
     If no output file is found for a test no output is assumed.
-    
+
     This yields (test_file, output_file) for each .impala file in the directory"""
     tests = []
 
@@ -286,7 +288,7 @@ def get_tests_for_file(file):
 
 def get_tests_from_dir(directory):
     testfile = os.path.join(directory, "tests.py")
-    
+
     if os.path.exists(testfile):
         tests = imp.load_source("tests", testfile).allTests()
     else:
@@ -295,7 +297,7 @@ def get_tests_from_dir(directory):
 
 def executeTests(tests, gEx):
     """Invoke this function with a list of test objects to run the tests. """
-    
+
     res = {}
     s   = True
     for i in range(len(tests)):
@@ -306,17 +308,17 @@ def executeTests(tests, gEx):
     failOpt = 0
     failReq = 0
     passOpt = []
-    
+
     opt_tests = []
     req_tests = []
     for t in tests:
         opt_tests.append(t) if t.isOptional() else req_tests.append(t)
-    
+
     for t in req_tests:
         if not res[t]:
             print("- REQUIRED test failed: "+t.getName())
             failReq += 1
-            
+
     for t in opt_tests:
         if not res[t]:
             print("- OPTIONAL test failed: "+t.getName())
@@ -326,7 +328,7 @@ def executeTests(tests, gEx):
 
     for test in passOpt:
         print("- OPTIONAL test passed: "+test)
-    
+
     if failOpt == 0 and failReq == 0:
         print("\n* All " + str(len(tests)) +  " tests were successful.")
     else:
@@ -338,5 +340,5 @@ def executeTests(tests, gEx):
             print("\n* All %i optional tests were successful." % len(opt_tests))
         else:
             print("\n!" + str(failOpt) + " of " + str(len(opt_tests)) + " OPTIONAL tests failed.")
-            
+
     return -1 if failReq > 0 else 0
