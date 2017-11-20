0e8a7b9d39309880d5444ba5176c2c6498fb7fde
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2017-11-20 18:06:22 +0100

    Test case for nested lambdas

---
+++ test/parser/positive/tests.py
@@ -0,0 +1,15 @@
+"""
+tests.py for parser tests
+"""
+
+# import the test infrastructure
+from infrastructure.tests import make_tests
+import os
+
+def allTests():
+    """
+    This function returns a list of tests for the parser.
+    """
+    
+    return make_tests("parser/positive", True)
+
