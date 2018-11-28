62fee5f20e6bd751818032f528b1496ee035cf1e
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2018-11-28 15:45:49 +0100

    Add check for arguments in run.py

--- test/run.py
+++ test/run.py
@@ -57,7 +57,7 @@ class test:
 
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class = argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('path', nargs='*',          help='path to test  or test directory',      default='./', type=str)
+    parser.add_argument('path', nargs='+',          help='path to test  or test directory',      default='./', type=str)
     parser.add_argument('-c',  '--clang',           help='path to clang binary',                 default=None, type=str)
     parser.add_argument('-i',  '--impala',          help='path to impala binary',                default=None, type=str)
     parser.add_argument('-it', '--impala-timeout',  help='timeout for compiling impala ',        default=5,    type=int)
