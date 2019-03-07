1f3e4ed22e91266f45d5c1f50f1c6b969e552e1c
Stefan Lemme <stefan.lemme@dfki.de>
2019-03-07 18:46:04 +0100

    Allow passing additional flags to clang

--- test/perform.py
+++ test/perform.py
@@ -83,12 +83,13 @@ class RunImpalaCompile(TestMethod):
         return True
 
 class LinkFakeRuntime(TestMethod):
-    def __init__(self, clang, runtime):
+    def __init__(self, clang, runtime, add_flags=[]):
         super().__init__(clang)
         self.runtime = runtime
+        self.flags = add_flags
 
     def __call__(self, testfile):
-        super().__call__([testfile.intermediate('.ll'), LIBC, self.runtime, "-o", testfile.intermediate(EXE)])
+        super().__call__([testfile.intermediate('.ll'), LIBC, self.runtime, "-o", testfile.intermediate(EXE)] + self.flags)
 
         self.dump_output(None)
 
@@ -201,6 +202,7 @@ if __name__ == '__main__':
     parser.add_argument('testfile',     nargs='+', help='path to one or multiple test files', type=argparse.FileType('r'))
     parser.add_argument('-i', '--impala',          help='path to impala',                     type=str, default=config.IMPALA_BIN)
     parser.add_argument('-c', '--clang',           help='path to clang',                      type=str, default=config.CLANG_BIN)
+    parser.add_argument(      '--clang-flag',      help='additional flag(s) for clang',       type=str, default='')
     parser.add_argument(      '--temp',            help='path to temp dir',                   type=str, default=config.TEMP_DIR)
     parser.add_argument(      '--rtmock',          help='path to rtmock',                     type=str, default=config.LIBRTMOCK)
     parser.add_argument('-t', '--compile-timeout', help='timeout for compiling test case',    type=int, default=5)
@@ -222,10 +224,12 @@ if __name__ == '__main__':
     FAILED = 1
     SKIPPED = 77
 
+    clang_flags = [arg.strip() for arg in args.clang_flag.split(' ')] if len(args.clang_flag) else []
+
     test_methods = {
         'codegen' : MultiStepPipeline(
             RunImpalaCompile(args.impala, timeout=args.compile_timeout),
-            LinkFakeRuntime(args.clang, args.rtmock),
+            LinkFakeRuntime(args.clang, args.rtmock, clang_flags),
             ExecuteTestOutput(timeout=args.run_timeout)
         )
     }
