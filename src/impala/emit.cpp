c9d2009007f0c392c6b4674af5cf6de59b7d3b37
Roland Leißa <leissa@cs.uni-saarland.de>
2019-06-17 21:06:42 +0200

    removed Var

--- src/impala/emit.cpp
+++ src/impala/emit.cpp
@@ -131,8 +131,6 @@ public:
 const thorin::Def* CodeGen::convert_rec(const Type* type) {
     if (auto lambda = type->isa<Lambda>()) {
         return world.lam(world.kind_star(), convert(lambda->body()), {lambda->name()});
-    } else if (auto var = type->isa<Var>()) {
-        return world.var(world.kind_star(), var->depth());
     } else if (auto prim_type = type->isa<PrimType>()) {
         switch (prim_type->primtype_tag()) {
 #define IMPALA_TYPE(itype, ttype) \
