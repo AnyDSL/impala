23fdb3729218922720a30b3083c406f52c3b0216
Roland Leißa <leissa@cs.uni-saarland.de>
2019-07-03 16:39:04 +0200

    adjusting to new API

--- src/impala/main.cpp
+++ src/impala/main.cpp
@@ -229,12 +229,14 @@ int main(int argc, char** argv) {
                         cg->emit(file, opt, debug);
                     }
                 };
-                emit_to_file(backends.cpu_cg.get(),    ".ll");
+                emit_to_file(backends.codegens[thorin::Backends::CPU].get(),    ".ll");
+#if 0
                 emit_to_file(backends.cuda_cg.get(),   ".cu");
                 emit_to_file(backends.nvvm_cg.get(),   ".nvvm");
                 emit_to_file(backends.opencl_cg.get(), ".cl");
                 emit_to_file(backends.amdgpu_cg.get(), ".amdgpu");
                 emit_to_file(backends.hls_cg.get(),    ".hls");
+#endif
 #else
                 thorin::outf("warning: built without LLVM support - I don't emit an LLVM file\n");
 #endif
