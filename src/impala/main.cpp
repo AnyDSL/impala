1fd6d2ad61bc126b357033657f9bfc0ac82e8005
Roland Leißa <leissa@cs.uni-saarland.de>
2021-01-27 04:25:05 +0100

    disable other non-cpu-llvm backends for now

--- src/impala/main.cpp
+++ src/impala/main.cpp
@@ -234,11 +234,11 @@ int main(int argc, char** argv) {
                     }
                 };
                 emit_to_file(backends.cpu_cg.get(),    ".ll");
-                emit_to_file(backends.cuda_cg.get(),   ".cu");
-                emit_to_file(backends.nvvm_cg.get(),   ".nvvm");
-                emit_to_file(backends.opencl_cg.get(), ".cl");
-                emit_to_file(backends.amdgpu_cg.get(), ".amdgpu");
-                emit_to_file(backends.hls_cg.get(),    ".hls");
+                //emit_to_file(backends.cuda_cg.get(),   ".cu");
+                //emit_to_file(backends.nvvm_cg.get(),   ".nvvm");
+                //emit_to_file(backends.opencl_cg.get(), ".cl");
+                //emit_to_file(backends.amdgpu_cg.get(), ".amdgpu");
+                //emit_to_file(backends.hls_cg.get(),    ".hls");
 #else
                 thorin::outf("warning: built without LLVM support - I don't emit an LLVM file\n");
 #endif
